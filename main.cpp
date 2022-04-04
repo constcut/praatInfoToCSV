cca5f02bff83dce542bc5d034c707caa4703d835
Konstantin Kutsenko <ssidein@gmail.com>
2022-04-04 13:10:38 +0300

    Preparation for watch-dog

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[])
 {
+
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
@@ -11,13 +12,20 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    //engine.rootContext()->setContextProperty("watchDog", &watchDog);
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+
+
     engine.load(url);
+
 
     return app.exec();
 }
